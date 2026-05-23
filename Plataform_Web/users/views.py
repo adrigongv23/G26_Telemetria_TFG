fd1c6250a466f52249376eb10a1c45b1cd5b3b68
adrigongv23 <adrigongv@gmail.com>
2026-05-23 12:16:38 +0200

    Frontend: estructuración del directorio y creación del archivo base

--- Plataform_Web/users/views.py
+++ Plataform_Web/users/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+@login_required # Esto obliga a que el usuario tenga que loguearse antes de ver la web
+def index(request):
+    # 'request.user' contiene los datos del usuario que está navegando
+    return render(request, 'index.html')
