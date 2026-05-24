cf862e56c78cd98c5e1727b5fdc1a97284925613
adrigongv23 <adrigongv@gmail.com>
2026-05-24 12:50:38 +0200

    Restructuración breve de la página principal

--- Plataform_Web/gades_manager/urls.py
+++ Plataform_Web/gades_manager/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 from gestion import views  # Importamos las vistas de la app donde guardaste la función
 
 urlpatterns = [
